65a776a7597a8f2803c21ee8c5f36f6e4da4e85f
Andres <Andriuvipip@protonmail.com>
2026-01-19 01:14:19 -0300

    super fix applied

--- bank.hpp
+++ bank.hpp
@@ -27,20 +27,26 @@ public:
 };
 
 class userlog {
-  std::random_device rd;
-  std::mt19937 gen(rd());
+private:
+  std::mt19937 idgen;
+  std::uniform_int_distribution<int> dist;
+  int zloty1;
 
-  std::uniform_int_distribution<> distrib(1, 1000);
+public:
+  userlog() : idgen(std::random_device{}()), dist(1, 1000) {
+    this->zloty1 = dist(idgen);
+  }
 
-  void DefaultUser(statusbank &k) {
+private:
+  void DefaultUser(statusbank &user) {
 
-    k.dineur = 100;
-    k.id = distrib(gen);
-    k.naam = "bit el insano ";
+    user.dineur = 100;
+    user.id = zloty1;
+    user.naam = "bit el insano ";
   }
   void userdef(statusbank &user) {
     user.dineur = 0;
-    user.id = distrib(gen);
+    user.id = zloty1;
     user.naam = "";
 
     std::cout << "Hola porfavor ingrese sus datos" << std::endl;
@@ -51,7 +57,7 @@ class userlog {
   }
 
 public:
-  void login() {
+  void login(statusbank &user) {
     std::cout << "deseas iniciar sesion tu mismo o te pongo un por defecto?(1 "
                  "para yo, 2 por defecto)"
               << std::endl;
@@ -60,14 +66,16 @@ public:
     if (confirmatio == 1) {
       userdef(user);
     } else {
-      DefaultUser(user k);
+      DefaultUser(user);
     }
   }
 };
 
 class billactions {
 
-  void agregardinero(statusbank &user, statusbank &k) {
+  void agregardinero(statusbank &user) {
+    statusbank e;
+    e.greatspace();
     while (true) {
       int option = 0;
       std::cout << "hola buenas cuanto dinero desea insertar?" << std::endl;
@@ -81,28 +89,24 @@ class billactions {
         std::cout << "Hola buenas tardes se te ha agregado 100$ a tu cuenta "
                   << std::endl;
         user.dineur += 100;
-        k.dineur += 100;
 
         continue;
       } else if (option == 2) {
         std::cout << "Hola buenas tardes se te ha agregado 300$ a tu cuenta "
                   << std::endl;
         user.dineur += 300;
-        k.dineur += 300;
 
         continue;
       } else if (option == 3) {
         std::cout << "Hola buenas tardes se te ha agregado 700$ a tu cuenta "
                   << std::endl;
         user.dineur += 700;
-        k.dineur += 700;
 
         continue;
       } else if (option == 4) {
         std::cout << "Hola buenas tardes se te ha agregado 1000$ a tu cuenta "
                   << std::endl;
         user.dineur += 1000;
-        k.dineur += 1000;
 
         continue;
       } else {
@@ -111,7 +115,10 @@ class billactions {
     }
   }
 
-  void sacardinero(statusbank &user, statusbank &k) {
+  void sacardinero(statusbank &user) {
+    statusbank e;
+    e.greatspace();
+
     while (true) {
       int option = 0;
       std::cout << "hola buenas cuanto dinero desea sacar?" << std::endl;
@@ -125,28 +132,24 @@ class billactions {
         std::cout << "Hola buenas tardes se te ha quitado  100$ de tu cuenta "
                   << std::endl;
         user.dineur -= 100;
-        k.dineur -= 100;
 
         continue;
       } else if (option == 2) {
         std::cout << "Hola buenas tardes se te ha quitado 300$ de tu cuenta "
                   << std::endl;
         user.dineur -= 300;
-        k.dineur -= 300;
 
         continue;
       } else if (option == 3) {
         std::cout << "Hola buenas tardes se te ha quitado 700$ de tu cuenta "
                   << std::endl;
         user.dineur -= 700;
-        k.dineur -= 700;
 
         continue;
       } else if (option == 4) {
         std::cout << "Hola buenas tardes se te ha quitado 1000$ de tu cuenta "
                   << std::endl;
         user.dineur -= 1000;
-        k.dineur -= 1000;
 
         continue;
       } else {
@@ -156,21 +159,25 @@ class billactions {
   }
 
 public:
-  void billinit() {
+  void billinit(statusbank &user) {
+    statusbank e;
+    e.greatspace();
+
     while (true) {
       int relevans = 0;
 
       std::cout << "hola buenas tardes usted desea sacar o agregar dinero "
-                   "a su cuenta? (1 para sacar, 2 para agregar,3 para si)"
+                   "a su cuenta? (1 para sacar, 2 para agregar,3 para salir)"
                 << std::endl;
       std::cin >> relevans;
       if (relevans == 1) {
 
-        sacardinero();
+        sacardinero(user);
+
         continue;
 
       } else if (relevans == 2) {
-        agregardinero();
+        agregardinero(user);
         continue;
 
       } else if (relevans == 3) {
@@ -185,21 +192,43 @@ public:
 };
 
 class menues {
-
+public:
   void init() {
-    userlog neptune;
-    billactions moon;
-    std::cout << "hola bienvenido al inicio de la cuenta de banco, por favor "
-                 "presione 1 para iniciar sesion, 0 para salir";
-    int conf = 0;
-    std::cin >> conf;
-    if (conf == 1) {
-      neptune.login();
-      moon.billinit();
+    while (true) {
+      statusbank user;
+      userlog neptune;
+      billactions moon;
+      statusbank r;
+      r.greatspace();
 
-    }
+      std::cout << "hola bienvenido al inicio de la cuenta de banco, por favor "
+                   "presione 1 para iniciar sesion, 0 para salir"
+                << std::endl;
+      int conf = 0;
+      std::cin >> conf;
+      if (conf == 1) {
+        neptune.login(user);
+        r.greatspace();
+        moon.billinit(user);
+        r.greatspace();
+        int conf2 = 0;
+        std::cout << "desea ver sus estadisticas? 1 para si 2 para no"
+                  << std::endl;
+        std::cin >> conf2;
+        if (conf2 == 1) {
+          std::cout << "su dinero actual es :" << user.dineur
+                    << " su id es: " << user.id
+                    << " y su nombre es :" << user.naam;
+          int conf3;
+          std::cout << "presione cualquier numero para continuar" << std::endl;
+          std::cin >> conf3;
+        }
+        continue;
+      }
 
-    else {
+      else {
+        break;
+      }
     }
   }
 };
--- main.cpp
+++ main.cpp
@@ -1,15 +1,8 @@
 #include "bank.hpp"
 #include <iostream>
 int main() {
-  std::cout << "Made by bit the constantinus  "<<std::endl;
-            << std::endl;
-  statusbank user;
-  statusbank k;
-  menues main;
-  userlog log;
-  billactions bill;
-  log.login(user);
-  bill.agregardinero(user);
-
+  std::cout << "Made by bit the constantinus  " << std::endl;
+  menues manis;
+  manis.init();
   return 0;
 }
